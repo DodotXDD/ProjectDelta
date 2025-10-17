59e3fdf0ecd6de1c4d6d11c1a86e9ddcd0d661b6
DodotXDD <dodot.lazarte@gmail.com>
2025-10-17 12:26:13 +0800

    First v1
     - Map
    - Player movement and view 
    - AI Movement

---
+++ bot.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+const SPEED = 4.0
+
+@export var player_path: NodePath
+@onready var player = get_node(player_path)
+@onready var nav_agent = $NavigationAgent3D
+
+func _process(delta):
+	if not player:
+		return
+		
+	# Move toward player
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	var direction = (next_nav_point - global_transform.origin).normalized()
+	velocity = direction * SPEED
+	move_and_slide()
+	
+	# Make the bot look at the player (Y-axis only)
+	var bot_pos = global_transform.origin
+	var player_pos = player.global_transform.origin
+	
+	player_pos.y = bot_pos.y  # Keep rotation flat (ignore vertical angle)
+	look_at(player_pos, Vector3.UP)
